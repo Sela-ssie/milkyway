06b17e53449c6101137e37975ec2a23496742b45
Selassie Ackah <sackah@login1.obshpc.carnegiescience.edu>
2026-08-16 16:14:34 -0700

    Refactor: Reorganize milkyway simulation directory structure with enhanced resource allocation for constrained baryonic runs

--- tree_gen_script.sh
+++ tree_gen_script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name="unconstrained_tree_gen_10k"
+#SBATCH --job-name="constrained_baryonic_tree_gen_10k"
 #SBATCH --nodes=1
-#SBATCH --mem=128G
-#SBATCH --cpus-per-task=48
-#SBATCH --error=unconstrained_output.log
-#SBATCH --output=unconstrained_output.log
-export GALACTICUS_DATA_PATH=/carnegie/nobackup/users/sackah/Galacticus/datasets
-/home/sackah/Galacticus/galacticus/Galacticus.exe /home/sackah/milkyway/constrainedMilkyWay.xml /home/sackah/milkyway/noLmcConstraint.xml
+#SBATCH --mem=256G
+#SBATCH --partition=dmtheory
+#SBATCH --cpus-per-task=64
+#SBATCH --error=constrained_baryonic_output_10k.log
+#SBATCH --output=constrained_baryonic_output_10k.log
+export GALACTICUS_DATA_PATH=/home/sackah/Galacticus/datasets
+/home/sackah/Galacticus/galacticus/Galacticus.exe /home/sackah/milkyway/parameter_files/baryonsMilkyWay.xml /home/sackah/milkyway/parameter_files/lmcConstraint.xml
